679077bcd5d594b49941eeb16b776dc8f107ce4f
Thakoor Chandan <thakoorchandan@gmail.com>
2021-12-18 18:11:23 +0530

    backend data added

--- src/controllers/aha.controller.js
+++ src/controllers/aha.controller.js
@@ -33,6 +33,12 @@ router.post("/ahaOriginals", async (req, res) => {
   return res.status(200).send(ahaOriginals);
 });
 
+// POST id --- Aha Originals
+router.post("/ahaOriginals/:id", async (req, res) => {
+  const ahaOriginals = await AhaOriginals.create(req.body);
+  return res.status(200).send(ahaOriginals);
+});
+
 // Get --- Aha Carousel
 router.get("/carousel", async (req, res) => {
   const ahaCarousel = await AhaCarousels.find().lean().exec();
@@ -47,6 +53,11 @@ router.get("/carousel/:id", async (req, res) => {
 
 // POST --- Aha Carousel
 router.post("/carousel", async (req, res) => {
+  const ahaCarousel = await AhaCarousels.create(req.body);
+  return res.status(200).send(ahaCarousel);
+});
+// POST id --- Aha Carousel
+router.post("/carousel/:id", async (req, res) => {
   const ahaCarousel = await AhaCarousels.create(req.body);
   return res.status(200).send(ahaCarousel);
 });
@@ -65,6 +76,11 @@ router.get("/action/:id", async (req, res) => {
 
 // POST --- Aha Action Movies
 router.post("/action", async (req, res) => {
+  const action = await AhaAction.create(req.body);
+  return res.status(200).send(action);
+});
+// POST --- Aha Action Movies
+router.post("/action/:id", async (req, res) => {
   const action = await AhaAction.create(req.body);
   return res.status(200).send(action);
 });
@@ -87,6 +103,12 @@ router.post("/handpicked", async (req, res) => {
   return res.status(200).send(handPicked);
 });
 
+// POST --- Aha HandPicked Movies
+router.post("/handpicked/:id", async (req, res) => {
+  const handPicked = await AhaHandPicked.create(req.body);
+  return res.status(200).send(handPicked);
+});
+
 // GET --- Aha Crime Movies
 router.get("/crime", async (req, res) => {
   const ahaCrime = await AhaCrime.find().lean().exec();
@@ -105,6 +127,12 @@ router.post("/crime", async (req, res) => {
   return res.status(200).send(ahaCrime);
 });
 
+// POST --- Aha Crime Movies
+router.post("/crime/:id", async (req, res) => {
+  const ahaCrime = await AhaCrime.create(req.body);
+  return res.status(200).send(ahaCrime);
+});
+
 // GET --- Aha Drama Movies
 router.get("/drama", async (req, res) => {
   const ahaDrama = await AhaDrama.find().lean().exec();
@@ -119,6 +147,12 @@ router.get("/drama/:id", async (req, res) => {
 
 // POST --- Aha Drama Movies
 router.post("/drama", async (req, res) => {
+  const ahaDrama = await AhaDrama.create(req.body);
+  return res.status(200).send(ahaDrama);
+});
+
+// POST --- Aha Drama Movies
+router.post("/drama/:id", async (req, res) => {
   const ahaDrama = await AhaDrama.create(req.body);
   return res.status(200).send(ahaDrama);
 });
@@ -143,6 +177,12 @@ router.post("/free_movies", async (req, res) => {
   return res.status(200).send(ahaFreeMovies);
 });
 
+// POST --- Aha Free Movies
+router.post("/free_movies/:id", async (req, res) => {
+  const ahaFreeMovies = await AhaFreeMovies.create(req.body);
+  return res.status(200).send(ahaFreeMovies);
+});
+
 // GET --- Aha Most Watched
 router.get("/most_watched", async (req, res) => {
   const ahaMostWatched = await AhaMostWatched.find().lean().exec();
@@ -159,6 +199,12 @@ router.get("/most_watched/:id", async (req, res) => {
 
 // POST --- Aha Most Watched
 router.post("/most_watched", async (req, res) => {
+  const ahaMostWatched = await AhaMostWatched.create(req.body);
+  return res.status(200).send(ahaMostWatched);
+});
+
+// POST --- Aha Most Watched
+router.post("/most_watched/:id", async (req, res) => {
   const ahaMostWatched = await AhaMostWatched.create(req.body);
   return res.status(200).send(ahaMostWatched);
 });
@@ -183,6 +229,12 @@ router.post("/new_release", async (req, res) => {
   return res.status(200).send(ahaNewReleases);
 });
 
+// POST --- Aha New Releases
+router.post("/new_release/:id", async (req, res) => {
+  const ahaNewReleases = await AhaNewReleases.create(req.body);
+  return res.status(200).send(ahaNewReleases);
+});
+
 // GET --- Aha Newly Added
 router.get("/newly_added", async (req, res) => {
   const ahaNewlyAdded = await AhaNewlyAdded.find().lean().exec();
@@ -199,6 +251,12 @@ router.get("/newly_added/:id", async (req, res) => {
 
 // POST --- Aha Newly Added
 router.post("/newly_added", async (req, res) => {
+  const ahaNewlyAdded = await AhaNewlyAdded.create(req.body);
+  return res.status(200).send(ahaNewlyAdded);
+});
+
+// POST --- Aha Newly Added
+router.post("/newly_added/:id", async (req, res) => {
   const ahaNewlyAdded = await AhaNewlyAdded.create(req.body);
   return res.status(200).send(ahaNewlyAdded);
 });
@@ -223,6 +281,12 @@ router.post("/romance", async (req, res) => {
   return res.status(200).send(ahaRomance);
 });
 
+// POST --- Aha Romance
+router.post("/romance/:id", async (req, res) => {
+  const ahaRomance = await AhaRomance.create(req.body);
+  return res.status(200).send(ahaRomance);
+});
+
 // GET --- Aha Thriller
 router.get("/thriller", async (req, res) => {
   const ahaThriller = await AhaThriller.find().lean().exec();
@@ -237,6 +301,12 @@ router.get("/thriller/:id", async (req, res) => {
 
 // POST --- Aha Thriller
 router.post("/thriller", async (req, res) => {
+  const ahaThriller = await AhaThriller.create(req.body);
+  return res.status(200).send(ahaThriller);
+});
+
+// POST --- Aha Thriller
+router.post("/thriller/:id", async (req, res) => {
   const ahaThriller = await AhaThriller.create(req.body);
   return res.status(200).send(ahaThriller);
 });
@@ -259,6 +329,12 @@ router.post("/top10", async (req, res) => {
   return res.status(200).send(ahaTop10);
 });
 
+// POST --- Aha Top 10
+router.post("/top10/:id", async (req, res) => {
+  const ahaTop10 = await AhaTop10.create(req.body);
+  return res.status(200).send(ahaTop10);
+});
+
 // GET --- Aha Upcoming
 router.get("/upcoming", async (req, res) => {
   const ahaUpcoming = await AhaUpcoming.find().lean().exec();
@@ -273,6 +349,12 @@ router.get("/upcoming/:id", async (req, res) => {
 
 // POST --- Aha Upcoming
 router.post("/upcoming", async (req, res) => {
+  const ahaUpcoming = await AhaUpcoming.create(req.body);
+  return res.status(200).send(ahaUpcoming);
+});
+
+// POST --- Aha Upcoming
+router.post("/upcoming/:id", async (req, res) => {
   const ahaUpcoming = await AhaUpcoming.create(req.body);
   return res.status(200).send(ahaUpcoming);
 });
